f5dafd63d4ce046877c0bee01914e7e79cc5124e
Thuotracy <tracyjacobs775@gmail.com>
2022-09-01 18:19:25 +0300

    created a function

---
+++ script.js
@@ -0,0 +1,21 @@
+function stars(){
+    let count = 500;
+    let scene = document.querySelector('.scene');
+    let i = 0;
+    while(i < count){
+        let star = document.createElement("i");
+        let x = Math.floor(Math.random() * window.innerWidth);
+        let y = Math.floor(Math.random() * window.innerHeight);
+        let duration = Math.random() * 10;
+        let size = Math.random() * 2;
+
+        star.style.left = x='px';
+        star.style.top = y+'px';
+        star.style.width = 1+size+'px';
+        star.style.height = 1+size+'px';
+
+        scene.appendChild(star);
+        i++
+    }
+}
+stars();
